7aed1d70c0028c5964d609ae95a0516d2f03cea4
ka10ryu1 <lsyp.tuiovjo.1231@gmail.com>
2018-04-23 21:08:54 +0900

    modified

--- predict.py
+++ predict.py
@@ -26,10 +26,10 @@ def command():
                         help='使用するモデルパラメータ')
     parser.add_argument('jpeg', nargs='+',
                         help='使用する画像のパス')
-    parser.add_argument('--img_rate', '-r', type=float, default=0.5,
-                        help='画像サイズの倍率 [default: 0.5]')
-    parser.add_argument('--batch', '-b', type=int, default=100,
-                        help='ミニバッチサイズ [default: 100]')
+    parser.add_argument('--out_rate', '-r', type=float, default=0.5,
+                        help='生成後の画像の拡大率 [default: 0.5]')
+    parser.add_argument('--batch', '-b', type=int, default=20,
+                        help='ミニバッチサイズ [default: 20]')
     parser.add_argument('--gpu', '-g', type=int, default=-1,
                         help='GPU ID [default -1]')
     parser.add_argument('--out_path', '-o', default='./result/',
@@ -37,40 +37,37 @@ def command():
     return parser.parse_args()
 
 
-def predict(model, data, size, rate, gpu):
+def predict(model, data, batch, org_shape, rate, gpu):
     """
     推論実行メイン部
     [in]  model:     推論実行に使用するモデル
     [in]  data:      分割（IMG.split）されたもの
     [in]  batch:     バッチサイズ
     [in]  org_shape: 分割前のshape
+    [in]  rate:      出力画像の拡大率
     [in]  gpu:       GPU ID
     [out] img:       推論実行で得られた生成画像
     """
 
-    cv2.imshow('test', data)
-    cv2.waitKey(0)
-    data_sq, _ = IMG.splitSQ(data, size, array=False)
-    data = IMG.resizeP(data_sq[0], size)
-    shape = data.shape
     # dataには圧縮画像と分割情報が含まれているので、分離する
+    comp, size = data
+    imgs = []
     st = time.time()
-    # 学習済みモデルに入力して画像を生成する
-    cv2.imshow('test', data)
-    cv2.waitKey(0)
-    x = IMG.img2arr(data, gpu=gpu)
-    x = x.reshape(1, x.shape[0], x.shape[1], x.shape[2])
-    y = model.predictor(x)
-    img = IMG.arr2img(to_cpu(y.array))
+    # バッチサイズごとに学習済みモデルに入力して画像を生成する
+    for i in range(0, len(comp), batch):
+        x = IMG.imgs2arr(comp[i:i + batch], gpu=gpu)
+        y = model.predictor(x)
+        imgs.extend(IMG.arr2imgs(to_cpu(y.array)))
+
     print('exec time: {0:.2f}[s]'.format(time.time() - st))
     # 生成された画像を分割情報をもとに結合する
-    # buf = [np.vstack(imgs[i * size[0]: (i + 1) * size[0]])
-    #        for i in range(size[1])]
-    # img = np.hstack(buf)
+    buf = [np.vstack(imgs[i * size[0]: (i + 1) * size[0]])
+           for i in range(size[1])]
+    img = np.hstack(buf)
     # 出力画像は入力画像の2倍の大きさになっているので半分に縮小する
     img = IMG.resize(img, 1 / rate)
     # 結合しただけでは画像サイズがオリジナルと異なるので切り取る
-    return img[:shape[0], :shape[1]]
+    return img[:org_shape[0], :org_shape[1]]
 
 
 def main(args):
@@ -83,9 +80,9 @@ def main(args):
         from Lib.network2 import JC_UDUD as JC
 
     model = L.Classifier(
-        JC(n_unit=unit, n_out=1,
-           rate=sr, actfun1=af1, actfun2=af2)  # , view=True)
+        JC(n_unit=unit, n_out=1, rate=sr, actfun1=af1, actfun2=af2)
     )
+
     # load_npzのpath情報を取得し、学習済みモデルを読み込む
     load_path = F.checkModelType(args.model)
     try:
@@ -101,27 +98,29 @@ def main(args):
         chainer.cuda.get_device_from_id(args.gpu).use()
         model.to_gpu()
 
-    # 学習モデルを入力画像ごとに実行する
-    ch_flg = IMG.getCh(ch)
-    org_imgs = [IMG.resize(cv2.imread(name, ch_flg), args.img_rate)
+    # 高圧縮画像の生成
+    org_imgs = [cv2.imread(name, IMG.getCh(ch))
                 for name in args.jpeg if IMG.isImgPath(name)]
-
     imgs = []
-    with chainer.using_config('train', False):
-        for i, ei in enumerate(org_imgs):
-            img = predict(model, ei, size, sr, args.gpu)
+    # 学習モデルを入力画像ごとに実行する
+    for i, ei in enumerate(org_imgs):
+        with chainer.using_config('train', False):
+            img = predict(
+                model, IMG.splitSQ(ei, size),
+                args.batch, ei.shape, sr, args.gpu
+            )
 
-            # 生成結果を保存する
-            name = F.getFilePath(args.out_path, 'comp-' +
-                                 str(i * 10 + 1).zfill(3), '.jpg')
-            print('save:', name)
-            cv2.imwrite(name, img)
-            imgs.append(img)
+        imgs.append(IMG.resize(img, args.out_rate))
 
-    # オリジナル、高圧縮、推論実行結果を連結して保存・表示する
-    for i in imgs:
-        cv2.imshow('test', i)
+    for img in imgs:
+        cv2.imshow('view', img)
         cv2.waitKey()
+        # 生成結果を保存する
+        name = F.getFilePath(
+            args.out_path, 'comp-' + str(i * 10 + 1).zfill(3), '.jpg'
+        )
+        print('save:', name)
+        cv2.imwrite(name, img)
 
 
 if __name__ == '__main__':
